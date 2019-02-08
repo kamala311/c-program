cd5d74c154774f857ce7b2a11c4afe92e34b2407
kamala311 <47448744+kamala311@users.noreply.github.com>
2019-02-08 23:48:53 +0530

    Create odd1.c

---
+++ odd1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a,b,i;
+scanf("%d %d",&a,&b);
+for(i=a;i<=b;i++)
+{
+    if(i!=1)
+    {
+        if(i%2!=0)
+    {
+        printf("%d ",i);
+    }
+    }
+}
+    return 0;
+}
