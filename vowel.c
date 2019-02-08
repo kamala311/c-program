2b40bbb05d1105ea1d6b2413691e08715eb2b1b5
kamala311 <47448744+kamala311@users.noreply.github.com>
2019-02-08 18:02:56 +0530

    Update and rename positive.c to vowel.c

---
+++ vowel.c
@@ -0,0 +1,15 @@
+# c-program
+
+int main() {
+	char s;
+	scanf("%c",&s);
+	if(s=='a'||s=='e'||s=='i'||s=='o'||s=='u'||s=='E'||s=='A'||s=='I'||s=='O'||s=='U')
+	{
+		printf("vowels");
+	}
+	else
+	{
+		printf("consonents");
+	}
+	return 0;
+}
