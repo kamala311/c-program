ee0328a077d02f0ca9b40bba63547b3271d30db7
kamala311 <47448744+kamala311@users.noreply.github.com>
2019-02-08 18:29:32 +0530

    Update alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,18 @@
+# c-program
+#include <stdio.h>
+
+
+int main() {
+	char s;
+    scanf("%c",&s);
+    if(s>='a' || s<='z')
+    {
+        printf("alphabet");
+        
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
+	]
